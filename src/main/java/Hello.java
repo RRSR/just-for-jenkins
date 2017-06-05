fcfd567a7887d74d79f02e1c833e299c200d053f
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-06-05 18:46:30 +0530

    Made it as a webapp.

--- src/main/java/Hello.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Raj Rathore on 05-Jun-17.
- */
-public class Hello {public static void main(String[] args) {
-      System.out.println("Hello World!!!");
-      System.out.println("Welcome");
-      System.out.println("OK BYE NOW");
-    }
-  }
